30676d475905303face7c3b0a8f76682cfa3348a
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-09-02 20:18:59 +0700

    masukan function pembaca file, return nya List<String>

--- lib/src/main/java/com/gzeinnumer/gzndirectory/FunctionGlobalFile.java
+++ lib/src/main/java/com/gzeinnumer/gzndirectory/FunctionGlobalFile.java
@@ -1,9 +1,13 @@
 package com.gzeinnumer.gzndirectory;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class FunctionGlobalFile {
 
@@ -17,6 +21,9 @@ public class FunctionGlobalFile {
         }
         if (!fileName.substring(0,1).equals("/")){
             fileName = "/"+fileName;
+        }
+        if (text.length==0){
+            return false;
         }
         File file = new File(FunctionGlobalDir.getStorageCard + FunctionGlobalDir.appFolder + fileName);
 
@@ -38,5 +45,36 @@ public class FunctionGlobalFile {
             e.printStackTrace();
             return false;
         }
+    }
+
+    public static List<String> readFile(String path){
+        List<String> list = new ArrayList<String>();
+        if (FunctionGlobalDir.appFolder.length()==0){
+            return list;
+        }
+        if (path.length()==0){
+            return list;
+        }
+        if (!path.substring(0,1).equals("/")){
+            path = "/"+path;
+        }
+        if (!FunctionGlobalDir.isFileExists(path)){
+            return list;
+        }
+
+        File file = new File(FunctionGlobalDir.getStorageCard+ path);
+        Scanner input;
+
+        try {
+            input = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return list;
+        }
+
+        while (input.hasNextLine()) {
+            list.add(input.nextLine());
+        }
+        return list;
     }
 }
