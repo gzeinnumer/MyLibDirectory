7fd6e571b9371fdc4e3bbbffdd73c0ffacc4def5
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-09-05 16:49:39 +0700

    appentText fnction

--- lib/src/main/java/com/gzeinnumer/gzndirectory/helper/FunctionGlobalFile.java
+++ lib/src/main/java/com/gzeinnumer/gzndirectory/helper/FunctionGlobalFile.java
@@ -3,6 +3,7 @@ package com.gzeinnumer.gzndirectory.helper;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -17,20 +18,20 @@ public class FunctionGlobalFile {
             FunctionGlobalDir.logSystemFunctionGlobal("initFile", "Folder External untuk aplikasi belum di deklarasi");
             return false;
         }
-        if (fileName.length()==0){
+        if (fileName.length() == 0) {
             FunctionGlobalDir.logSystemFunctionGlobal("initFile", "FileName tidak boleh kosong");
             return false;
         }
-        if (!fileName.substring(0,1).equals("/")){
-            fileName = "/"+fileName;
+        if (!fileName.substring(0, 1).equals("/")) {
+            fileName = "/" + fileName;
         }
-        if (text.length==0){
-            FunctionGlobalDir.logSystemFunctionGlobal("initFile", "Tidak ada text yang akan dikirim");
-            return false;
-        }
+//        if (text.length==0){
+//            FunctionGlobalDir.logSystemFunctionGlobal("initFile", "Tidak ada text yang akan dikirim");
+//            return false;
+//        }
         File file = new File(FunctionGlobalDir.getStorageCard + FunctionGlobalDir.appFolder + fileName);
 
-        return  processFile(file, text);
+        return processFile(file, text);
     }
 
     private static boolean processFile(File file, String... text) {
@@ -84,5 +85,38 @@ public class FunctionGlobalFile {
             list.add(input.nextLine());
         }
         return list;
+    }
+
+    public static boolean appentText(String path, String msg) {
+        if (FunctionGlobalDir.appFolder.length() == 0) {
+            FunctionGlobalDir.logSystemFunctionGlobal("appentText", "Folder External untuk aplikasi belum dideklarasi");
+            return false;
+        }
+        if (path.length() == 0) {
+            FunctionGlobalDir.logSystemFunctionGlobal("appentText", "Path tidak boleh kosong");
+            return false;
+        }
+        if (!path.substring(0, 1).equals("/")) {
+            path = "/" + path;
+        }
+        if (!FunctionGlobalDir.isFileExists(FunctionGlobalDir.appFolder + FunctionGlobalDir.appFolder + path)) {
+            FunctionGlobalDir.logSystemFunctionGlobal("appentText", "File tidak ditemukan");
+            return false;
+        }
+        if (msg.length() == 0) {
+            FunctionGlobalDir.logSystemFunctionGlobal("appentText", "Message tidak boleh kosong");
+            return false;
+        }
+        FileWriter fw;
+        try {
+            fw = new FileWriter(FunctionGlobalDir.getStorageCard + FunctionGlobalDir.appFolder + path, true);
+            fw.write(msg + "\n");
+            fw.close();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            FunctionGlobalDir.logSystemFunctionGlobal("appentText", "Gagal mengapent text ke file " + e.getMessage());
+            return false;
+        }
     }
 }
